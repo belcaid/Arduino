af420088b86dad1ad5332cbdbd6b32b9caaa21c8
belcaid <belcaid.mehdi@gmail.com>
2015-05-17 14:35:40 -0400

    Update allumage-led.ino

--- Allumage-led/allumage-led.ino
+++ Allumage-led/allumage-led.ino
@@ -1,14 +1,14 @@
-int led = 9; // Pin connectee a la led
+int led = 9; // Déclaration de la Borche connectee a la led
 
-void setup() {
+void setup() { // Fonction d'intialisation 
   
-pinMode(led,OUTPUT);
+pinMode(led,OUTPUT);  // La broche en sortie 
 }
 
 void loop() {
 
-digitalWrite(led,HIGH);
-delay(1000);
-digitalWrite(led,LOW);
-delay(1000);
+digitalWrite(led,HIGH); // La broche à 5v pour allumer la led
+delay(1000);            // Pause d'une seconde (1000 ms)
+digitalWrite(led,LOW);  // La broche à 0v pour éteindre la led
+delay(1000);            // Pause de 1000 millisecondes
 }
